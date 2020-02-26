92c32e7725acbc427a8858ad97433f4fb7170c0e
Dragneel2710 <61239842+Dragneel2710@users.noreply.github.com>
2020-02-26 20:12:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var R1;
+var R2;
+
+function setup() {
+  createCanvas(1200,800);
+  R1 = createSprite(200, 200, 50, 80);
+  R2 = createSprite(400,200,80,30);
+}
+
+function draw() {
+  background(0,0,0);  
+
+ R2.x = World.mouseX;
+ R2.y = World.mouseY;
+
+ console.log(R1.width/2 + R2.width/2);
+
+ if (R2.x - R1.x < R1.width/2 + R2.width/2 && R1.x - R2.x < R2.width/2 + R1.width/2 && R2.y - R1.y < R1.height/2 + R2.height/2 && R1.y - R2.y < R2.height/2 + R1.height/2 ){
+   R1.shapeColor = "Blue";
+   R2.shapeColor = "Red";
+ }
+
+ else {
+   R1.shapeColor = "Grey";
+   R2.shapeColor = "Grey";
+ }
+
+  drawSprites();
+}
